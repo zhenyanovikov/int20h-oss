a670f6d6407225bc9ffa6f86e9d4487a69b66ebc
yevhenii.novikov <zhenyanovikov.dev@gmail.com>
2024-01-29 00:41:15 +0200

    update update.sh

--- update.sh
+++ update.sh
@@ -9,4 +9,4 @@ db_password=svXsnZVVUUybzNwgdVGWJpgYFzchBw db_port=5432 make migrate-up
 commit_message=$(git log -1 --pretty=%B | jq -sRr @uri)
 
 # Send the message via Telegram using a GET request
-curl "https://api.telegram.org/bot6986345040:AAFQRp67kGLdns3q1q86s2_fVkC6_Er55F0/sendMessage?chat_id=-1002084258380&text=oss%20deployed%3A%20$commit_message"
+#curl "https://api.telegram.org/bot6986345040:AAFQRp67kGLdns3q1q86s2_fVkC6_Er55F0/sendMessage?chat_id=-1002084258380&text=oss%20deployed%3A%20$commit_message"
