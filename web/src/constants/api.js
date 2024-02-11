a5f0918667befa4fe205e5018a75b64e616e4381
yevhenii.novikov <zhenyanovikov.dev@gmail.com>
2024-02-11 22:11:59 +0200

    update base_url

--- web/src/constants/api.js
+++ web/src/constants/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://oss.yevhenii.live:8081/api/v1";
+export const BASE_URL = "https://api.blago.yevhenii.live/api/v1";
 export const MOCKED_BASE_URL =
   "https://802b42df-8d93-49c8-83a4-e8b74810e571.mock.pstmn.io";
